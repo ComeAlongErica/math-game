cf8f41779a4c26ed367c7a77cbb1b6e6327bf1c2
Erica Perry <erica.perry@woodlanddirect.com>
2020-03-13 17:05:26 -0400

    create functions for getting data from the forms

--- app/app.ts
+++ app/app.ts
@@ -1,15 +1,25 @@
 function startGame() {
   // start a new game
   let playerName: string | undefined;
-  playerName = undefined
+  playerName = getInputValue('playername')
   typeof playerName === 'string' && logPLayer(playerName)
 
   const messagesElement = document.getElementById('messages')
   messagesElement!.innerText = 'Welcome! Starting a new game...'
 }
 
-function logPLayer(name: string) {
+function logPLayer(name: string): void {
   console.log(`New game is starting for played: ${name}`)
+}
+
+function getInputValue(elementId: string): string | undefined {
+  const inputElm: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId)
+  return inputElm ? inputElm.value : undefined
+}
+
+function postScore(score: number, player: string): void {
+  const inputElm: HTMLElement = <HTMLElement>document.getElementById('postedScores')
+  inputElm!.innerText = `${player}: ${score}`
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame)
