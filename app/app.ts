2bc48f8465bf4092f050634b5ae14d31a7895b8e
Erica Perry <erica.perry@woodlanddirect.com>
2020-03-13 16:34:14 -0400

    write function using type annotations

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,13 @@
 function startGame() {
   // start a new game
+  const playerName: string = 'Audrey'
+  logPLayer(playerName)
   const messagesElement = document.getElementById('messages')
   messagesElement!.innerText = 'Welcome! Starting a new game...'
+}
+
+function logPLayer(name: string) {
+  console.log(`New game is starting for played: ${name}`)
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame)
