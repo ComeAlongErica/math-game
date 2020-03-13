cd12be5df9293ae6002e022620a475eb7ac75e97
Erica Perry <erica.perry@woodlanddirect.com>
2020-03-13 17:12:00 -0400

    add more type annotations and default params

--- app/app.ts
+++ app/app.ts
@@ -2,22 +2,23 @@ function startGame() {
   // start a new game
   let playerName: string | undefined;
   playerName = getInputValue('playername')
-  typeof playerName === 'string' && logPLayer(playerName)
+  logPLayer(playerName)
+  postScore(100)
 
   const messagesElement = document.getElementById('messages')
   messagesElement!.innerText = 'Welcome! Starting a new game...'
 }
 
-function logPLayer(name: string): void {
+function logPLayer(name: string = 'Player'): void {
   console.log(`New game is starting for played: ${name}`)
 }
 
 function getInputValue(elementId: string): string | undefined {
   const inputElm: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId)
-  return inputElm ? inputElm.value : undefined
+  return inputElm.value === '' ? undefined : inputElm.value
 }
 
-function postScore(score: number, player: string): void {
+function postScore(score: number, player: string = 'Player'): void {
   const inputElm: HTMLElement = <HTMLElement>document.getElementById('postedScores')
   inputElm!.innerText = `${player}: ${score}`
 }
